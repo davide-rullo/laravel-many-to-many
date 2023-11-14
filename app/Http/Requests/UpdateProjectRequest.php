dd0891738d903bee11d9cc598e8f23d57d589792
davide-rullo <davide.rullo01@gmail.com>
2023-11-14 13:50:12 +0100

    initial commit new repo

---
+++ app/Http/Requests/UpdateProjectRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rule;
+
+class UpdateProjectRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return Auth::id() === 1;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => ['required', 'min:3', Rule::unique('projects')->ignore($this->project)],
+            'cover_image' => ['nullable'],
+            'type_id' => ['nullable', 'exists:types,id'],
+            'description' => ['nullable'],
+            'github_link' => ['nullable'],
+            'online_link' => ['nullable']
+
+        ];
+    }
+}
